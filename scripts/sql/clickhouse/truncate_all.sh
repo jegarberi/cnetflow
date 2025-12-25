8fd8fcc0f9a63443e5377121cc963dcbcd81683b
Juan Garberi <jegarberi@gmail.com>
2025-12-25 19:49:35 -0300

    add truncate script

---
+++ scripts/sql/clickhouse/truncate_all.sh
@@ -0,0 +1,6 @@
+truncate "netflow"."flows_5minute_mv";
+truncate "netflow"."flows";
+truncate "netflow"."flows_daily_mv";
+truncate "netflow"."flows_dstport_hourly_mv";
+truncate "netflow"."flows_protocol_hourly_mv";
+truncate "netflow"."flows_top_talkers_hourly_mv";
