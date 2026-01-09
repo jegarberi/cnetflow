04db73478728f961e40de570d4aefbd6fffeec42
Juan Garberi <jegarberi@gmail.com>
2026-01-09 19:25:48 -0300

    update `_MAX_OCTETS_TO_CONSIDER_WRONG` and `_MAX_PACKETS_TO_CONSIDER_WRONG` definitions to lower thresholds

--- src/db_clickhouse.h
+++ src/db_clickhouse.h
@@ -4,8 +4,8 @@
 
 #ifndef DB_CLICKHOUSE_H
 #define DB_CLICKHOUSE_H
-#define _MAX_OCTETS_TO_CONSIDER_WRONG 10737418240 //10G
-#define _MAX_PACKETS_TO_CONSIDER_WRONG 10737418240
+#define _MAX_OCTETS_TO_CONSIDER_WRONG 500000000
+#define _MAX_PACKETS_TO_CONSIDER_WRONG 5000000
 #include <curl/curl.h>
 #include "netflow.h"
 #include <stdint.h>
